e5549c4a497e173751907700b5ba4206e8921759
tastybento <tastybento@users.noreply.github.com>
2025-11-08 08:48:26 -0800

    Made chunk loading async

--- src/main/java/world/bentobox/stranger/listeners/NetherChunkMaker.java
+++ src/main/java/world/bentobox/stranger/listeners/NetherChunkMaker.java
@@ -7,6 +7,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
+import java.util.Set;
 import java.util.TreeMap;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
@@ -49,6 +50,7 @@ import world.bentobox.bentobox.api.user.User;
 import world.bentobox.bentobox.database.Database;
 import world.bentobox.bentobox.util.ExpiringSet;
 import world.bentobox.bentobox.util.Pair;
+import world.bentobox.bentobox.util.Util;
 import world.bentobox.stranger.StrangerRealms;
 import world.bentobox.stranger.database.NetherChunksMade;
 
@@ -57,15 +59,13 @@ import world.bentobox.stranger.database.NetherChunksMade;
  */
 public class NetherChunkMaker implements Listener {
 
-    private static final int ROOF_HEIGHT = 107;
+    private static final int ROOF_HEIGHT = 107; 
+    private static final int CEILING_START = 100; // Blocks above this height are left as natural nether blocks
+    private static final int NETHER_FLOOR = 7; // Blocks below this are left as natural nether world blocks
     private static final String NETHER_CHUNKS_TABLE = "NetherChunks";
-    private StrangerRealms addon;
-    private Random rand = new Random();
-    private final Database<NetherChunksMade> handler;
-    private NetherChunksMade netherChunksMade;
-    private final int maxChestFills;
-    private ExpiringSet<UUID> portalPlayer = new ExpiringSet<>(10, TimeUnit.SECONDS);
-    private final TreeMap<Integer, LootTable> chestContents;
+    private static final Set<Material> NETHER_STRUCTURE_BLOCKS = Set.of(Material.NETHER_BRICKS, Material.NETHER_BRICK_FENCE, 
+            Material.NETHER_BRICK_SLAB, Material.NETHER_BRICK_STAIRS, Material.NETHER_BRICK_WALL, Material.CHISELED_NETHER_BRICKS
+            , Material.CRACKED_NETHER_BRICKS, Material.BONE_BLOCK, Material.SPAWNER, Material.CHEST);
 
     // Overworld Biome -> Nether Biome
     public static final Map<Biome, Biome> BIOME_MAPPING;
@@ -100,10 +100,23 @@ public class NetherChunkMaker implements Listener {
 
         BIOME_MAPPING = Collections.unmodifiableMap(biomeMap);
     }
+    
+    private StrangerRealms addon;
+    private Random rand = new Random();
+    private final Database<NetherChunksMade> handler;
+    private NetherChunksMade netherChunksMade;
+    private final int maxChestFills;
+    private ExpiringSet<UUID> portalPlayer = new ExpiringSet<>(10, TimeUnit.SECONDS);
+    private final TreeMap<Integer, LootTable> chestContents;
 
+    /**
+     * Class that transforms the nether to the upside down
+     * @param addon Addon
+     */
     public NetherChunkMaker(StrangerRealms addon) {
         super();
         this.addon = addon;
+        // Create the database table where we will log what chunks have been transformed
         handler = new Database<>(addon, NetherChunksMade.class);
         if (handler.objectExists(NETHER_CHUNKS_TABLE)) {
             netherChunksMade = handler.loadObject(NETHER_CHUNKS_TABLE);
@@ -112,7 +125,7 @@ public class NetherChunkMaker implements Listener {
             netherChunksMade = new NetherChunksMade();
             handler.saveObjectAsync(netherChunksMade);
         }
-        // Chests
+        // Chests and their potential contents
         maxChestFills = addon.getSettings().getChestFills();
         chestContents = new TreeMap<>();
         chestContents.put(1, LootTables.BASTION_TREASURE.getLootTable()); // Rare
@@ -132,7 +145,7 @@ public class NetherChunkMaker implements Listener {
 
     /**
      * Handles the event when a player enters a Nether portal.
-     * 
+     * This event can be called multiple times as the player goes through.
      * @param e The event triggered when an entity enters a portal.
      */
     @EventHandler(priority = EventPriority.NORMAL, ignoreCancelled = true)
@@ -145,7 +158,7 @@ public class NetherChunkMaker implements Listener {
             return;
         }
         Player p = (Player)e.getEntity();
-        // Add the player as teleporting
+        // Flag the player as teleporting to debounce the additional event calls
         portalPlayer.add(p.getUniqueId());
         if (e.getLocation().getWorld().getEnvironment() == Environment.NETHER) {
             return; // No action needed if already in the Nether
@@ -160,7 +173,7 @@ public class NetherChunkMaker implements Listener {
     }
 
     /**
-     * Handles the event when a player enters the UpsideDown.
+     * Handles the event when a player enters the UpsideDown via a teleport.
      * 
      * @param e The event triggered
      */
@@ -173,7 +186,8 @@ public class NetherChunkMaker implements Listener {
 
     /**
      * Refreshes the Nether chunks based on the player's location.
-     * 
+     * This removes the database entries of chunks made so that when the chunk is
+     * reloaded next time it will be transformed again.
      * @param p The player who triggered the refresh.
      */
     private void refreshNetherChunks(Player p) {
@@ -231,8 +245,12 @@ public class NetherChunkMaker implements Listener {
         handler.saveObjectAsync(netherChunksMade); // Save to database
         clearTileEntities(e); // Clear tile entities in the chunk
         clearEntities(e); // Clear entities in the chunk
-        convertBlocks(e); // Convert blocks from overworld to Nether
-        spawnNetherEntities(e); // Spawn Nether entities
+        // Get the overworld chunk async
+        Util.getChunkAtAsync(addon.getOverWorld(), e.getChunk().getX(), e.getChunk().getZ()).thenRun(() -> {
+            convertBlocks(e);
+            convertBlocks(e); // Convert blocks from overworld to Nether
+            spawnNetherEntities(e); // Spawn Nether entities
+        });
     }
 
     /**
@@ -288,29 +306,18 @@ public class NetherChunkMaker implements Listener {
         // Check for structures
         List<BoundingBox> structures = new ArrayList<>();
         e.getChunk().getStructures().forEach(gs -> structures.add(gs.getBoundingBox()));
-
         // Loop through the chunk and set blocks
-        for (int y = e.getWorld().getMinHeight() + 7; y < ROOF_HEIGHT; y++) {
+        for (int y = e.getWorld().getMinHeight() + NETHER_FLOOR; y < ROOF_HEIGHT; y++) {
             for (int x = 0; x < 16; x++) {
                 for (int z = 0; z < 16; z++) {
                     Block overworldBlock = overworldChunk.getBlock(x, y, z);
                     Block newBlock = e.getChunk().getBlock(x, y, z);
                     newBlock.setBiome(BIOME_MAPPING.getOrDefault(overworldBlock.getBiome(), Biome.NETHER_WASTES)); // Set biome for the new block
-                    if (newBlock.getBiome() == Biome.DEEP_DARK
-                            || overworldBlock.getType() == newBlock.getType() 
+                    if (overworldBlock.getType() == newBlock.getType() 
                             || newBlock.getType() == Material.NETHER_PORTAL // We must not touch these otherwise errors occur
-                            || y > 100 && rand.nextDouble() < attrition
-                            || (inStructure(e.getChunk().getX(), e.getChunk().getZ(), x, y, z, structures) && (newBlock.getType() == Material.NETHER_BRICKS
-                            || newBlock.getType() == Material.NETHER_BRICK_FENCE
-                            || newBlock.getType() == Material.NETHER_BRICK_SLAB
-                            || newBlock.getType() == Material.NETHER_BRICK_STAIRS
-                            || newBlock.getType() == Material.NETHER_BRICK_WALL
-                            || newBlock.getType() == Material.CHISELED_NETHER_BRICKS
-                            || newBlock.getType() == Material.CRACKED_NETHER_BRICKS
-                            || newBlock.getType() == Material.BONE_BLOCK
-                            || newBlock.getType() == Material.SPAWNER
-                            || newBlock.getType() == Material.CHEST))            
-                            ) {
+                            || y > CEILING_START && rand.nextDouble() < attrition
+                            || (inStructure(e.getChunk().getX(), e.getChunk().getZ(), x, y, z, structures) 
+                                    &&  NETHER_STRUCTURE_BLOCKS.contains(newBlock.getType()))) {
                         continue; // Skip if the block types are the same or if conditions are met
                     }
                     BlockData overworldBlockData = overworldBlock.getBlockData();
